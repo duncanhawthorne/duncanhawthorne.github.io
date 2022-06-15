da326d52673e9c5dd253bcc03408ad0bb16e4925
duncanhawthorne <duncan.hawthorne@gmail.com>
2022-06-15 22:27:49 +0100

    Update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -18,9 +18,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "427a24cd1e266bc17f2c43029a09601b",
 "icons/Icon-maskable-192.png": "8ab6f2e8d6d6e704dea86cac7238f577",
 "icons/Icon-maskable-512.png": "427a24cd1e266bc17f2c43029a09601b",
-"index.html": "5e3d6fecb4440b1fc1b953434298013e",
-"/": "5e3d6fecb4440b1fc1b953434298013e",
-"main.dart.js": "2276021354bb74cd2de754841d6db88e",
+"index.html": "0444d6add4f7d3f800ad36677e337a44",
+"/": "0444d6add4f7d3f800ad36677e337a44",
+"main.dart.js": "145b5b0a0b605a21a4a5663a33bf18ed",
 "manifest.json": "d84d606b909c34571744b3b0f1468f3f",
 "version.json": "aa5ad8cb591a3f99cfceca26fb675979"
 };
